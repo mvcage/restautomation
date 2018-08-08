91d6d2b9b2eb0fa373f3ca319f36da3486853026
Mikhail <mvcagegamedev@gmail.com>
2018-08-08 10:50:25 +0300

    Логирование результатов

--- src/test/java/example/service01/CreateUser.java
+++ src/test/java/example/service01/CreateUser.java
@@ -24,38 +24,6 @@ import org.junit.runner.Description;
 
 public class CreateUser extends MainTestClass {
 
-    /*private static StringBuilder builder = new StringBuilder();
-    @AfterClass
-    public static void afterClass() throws IOException {
-        PrintWriter logFile = new PrintWriter("C:\\Program Files (x86)\\Jenkins\\jobs\\AutomationREST\\TestResult.xml", "UTF-8");
-        logFile.write(builder.toString());
-        logFile.close();
-    }
-
-    @Rule
-    public TestWatcher watchman = new TestWatcher() {
-
-        @Override
-        protected void failed(Throwable e, Description description) {
-            if (description != null) {
-                builder.append(description);
-            }
-            if (e != null) {
-                builder.append(' ');
-                builder.append(e);
-            }
-            builder.append(" FAIL\n");
-        }
-
-        @Override
-        protected void succeeded(Description description) {
-            if (description != null) {
-                builder.append(description);
-            }
-            builder.append(" OK\n");
-        }
-    };*/
-
     @Test
     @Category({RegressionCat.class})
 
